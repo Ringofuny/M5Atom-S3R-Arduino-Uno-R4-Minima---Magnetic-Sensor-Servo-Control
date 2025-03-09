c142b9837a7f3adde741445b2f19001580ef2cf7
Ringofuny <167602344+Ringofuny@users.noreply.github.com>
2025-03-09 09:57:58 +0900

    サーボ駆動部分を追加

--- ArduinoUno_R4_MINIMA/MAG/receive/src/main.cpp
+++ ArduinoUno_R4_MINIMA/MAG/receive/src/main.cpp
@@ -1,5 +1,8 @@
 #include "data_recovery.h"
 #include "angle.h"
+#include <Servo.h>
+Servo servo;
+Servo back_servo;
 
 RECEIVE_DATA received;
 data_recovery recovery;
@@ -10,12 +13,15 @@ int fail = 0;
 void setup() {
     Serial1.begin(38400);
     Serial.begin(115200);
+    servo.attach(9);
+    back_servo.attach(10);
 }
 
 void loop() {
     bool update_state = recovery.getRecoveryData(&received.mag.x, &received.mag.y, &received.mag.z);
     if (update_state) {
         fail = 0;
+        float angle = getAngle(received.mag.x, received.mag.y);
         Serial.print("OK x: ");
         Serial.print(received.mag.x);
         Serial.print(" y: ");
@@ -23,9 +29,24 @@ void loop() {
         Serial.print(" z: ");
         Serial.print(received.mag.z);
         Serial.print(" angle: ");
-        Serial.println(getAngle(received.mag.x, received.mag.y));
+        Serial.println(angle);
+        servo.write(angle);
+        back_servo.write(fabs(180-angle));
     } else {
         fail++;
     } 
     if (fail > 10) Serial.println("fail");
 }
+
+/*
+    |-----------|
+    | 2       1 |
+    |           |
+    |           |
+    | 3       4 |
+    |-----------|
+
+    1,2 -> back_servo
+    3,4 -> servo
+
+*/
